2edd2dcaa472007245fc0334ec096d46b78ce3e9
ILhankhondaker <ilhankhondaker@gmail.com>
2025-01-05 11:10:58 +0600

    add hero section

---
+++ src/components/shared/SectionHeading/SectionHeading.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface Props {
+    heading: string;
+    subheading: string;
+}
+
+const SectionHeading = ({ heading, subheading }: Props) => {
+  return (
+    <div className='my-5  w-full mx-auto'>
+            <p className='text-center text-5xl text-primary-green-hover font-bold'> {subheading} </p>
+            <h1 className='text-center font-medium text-xl lg:text-3xl text-primary-green-hover my-2'>{heading}</h1>
+            
+          <Image
+          className='mx-auto'
+                src="/assets/img/Line.png"
+                width={80}
+                height={80}
+                alt="Picture of the author"
+              />
+        </div>
+  )
+}
+
+export default SectionHeading
+
+// Use it where needed
+
+  // <SectionHeading subheading={"Shop By Popular categories"} heading={"Popular categories"} />
+
