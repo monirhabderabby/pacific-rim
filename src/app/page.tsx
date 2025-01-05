4bd36013a9edc2514840f4d5505b65d861675ef5
sozibhossain <sozibhossain69@gmail.com>
2025-01-05 14:00:19 +0600

    newsletter add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 import TopHeader from "@/components/shared/header/topHeader/page";
 import MainHeader from "@/components/shared/header/mainHeader/page";
+import MainFooter from "@/components/shared/footer/mainFooter/page";
+import Newsletter from "@/components/shared/footer/newsletter/page";
 
 // ilhan 
 
@@ -10,6 +12,9 @@ export default function Home() {
       <TopHeader />
       <MainHeader />
       <h1>ok</h1>
+
+      <Newsletter />
+      <MainFooter />
     </>
   );
 }
