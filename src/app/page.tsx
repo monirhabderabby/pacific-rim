e1e1fb98c2149cadb555fe9ef5449c03478fa2b9
ILhankhondaker <ilhankhondaker@gmail.com>
2025-01-05 14:11:34 +0600

    add dynamic section heading  & popular categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero/Hero";
+import PopularCategories from "@/components/PopularCategories/PopularCategories";
 
 // ilhan 
 
@@ -7,6 +8,7 @@ export default function Home() {
   <>
  
 <Hero/>
+<PopularCategories/>
   </>
   );
 }
