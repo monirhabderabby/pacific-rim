02a35b7f3df81bff769dfe841538fcb293ad4b6b
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-09 15:08:44 +0600

    feat: auction navbar

--- src/components/shared/header/mainHeader/navbar.tsx
+++ src/components/shared/header/mainHeader/navbar.tsx
@@ -19,15 +19,47 @@ interface DesktopNavbarProps {
 }
 
 const Navicons = [
-  { href: "/notifications", src: "/assets/svg/bell.svg", alt: "bell-icon", count: 4, srOnlyText: "View notifications" },
-  { href: "/wishlist", src: "/assets/svg/heart.svg", alt: "heart-icon", srOnlyText: "View wishlist" },
-  { href: "/cart", src: "/assets/svg/cart-icon.svg", alt: "cart-icon", count: 2, srOnlyText: "View cart" },
-  { href: "/account", src: "/assets/svg/user-circle.svg", alt: "user-icon", srOnlyText: "View account" },
-
+  {
+    href: "/notifications",
+    src: "/assets/svg/bell.svg",
+    alt: "bell-icon",
+    count: 4,
+    srOnlyText: "View notifications",
+  },
+  {
+    href: "/wishlist",
+    src: "/assets/svg/heart.svg",
+    alt: "heart-icon",
+    srOnlyText: "View wishlist",
+  },
+  {
+    href: "/cart",
+    src: "/assets/svg/cart-icon.svg",
+    alt: "cart-icon",
+    count: 2,
+    srOnlyText: "View cart",
+  },
+  {
+    href: "/account",
+    src: "/assets/svg/user-circle.svg",
+    alt: "user-icon",
+    srOnlyText: "View account",
+  },
 ];
 const mobileNavicons = [
-  { href: "/wishlist", src: "/assets/svg/heart.svg", alt: "heart-icon",  srOnlyText: "View wishlist" },
-  { href: "/cart", src: "/assets/svg/cart-icon.svg", alt: "cart-icon", count: 4, srOnlyText: "View cart" },
+  {
+    href: "/wishlist",
+    src: "/assets/svg/heart.svg",
+    alt: "heart-icon",
+    srOnlyText: "View wishlist",
+  },
+  {
+    href: "/cart",
+    src: "/assets/svg/cart-icon.svg",
+    alt: "cart-icon",
+    count: 4,
+    srOnlyText: "View cart",
+  },
 ];
 
 function DesktopNavbar({ pathName, loggedin }: DesktopNavbarProps) {
@@ -62,7 +94,7 @@ function DesktopNavbar({ pathName, loggedin }: DesktopNavbarProps) {
           About
         </Link>
         <Link
-          href="/products"
+          href=""
           className={cn(
             "text-sm/6 font-semibold hover:text-primary-green",
             pathName === "/products" ? "text-primary-green" : "text-gray-900"
@@ -111,20 +143,20 @@ function MobileTabletNavbar({ loggedin }: { loggedin: boolean }) {
 
   return (
     <>
-    <div className="flex items-center justify-between h-[56px] px-4">
-       <div className="flex ">
-        <Link href="/" className="-m-1.5 p-1.5">
-          <span className="sr-only">Pacific Rim</span>
-          <Image alt="" src={HeaderLogo} className="h-10 w-20" />
-        </Link>
-      </div>
-      <div className="flex items-center gap-4">
-        <HeaderIconMenu icons={mobileNavicons}/>
+      <div className="flex items-center justify-between h-[56px] px-4">
+        <div className="flex ">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Pacific Rim</span>
+            <Image alt="" src={HeaderLogo} className="h-10 w-20" />
+          </Link>
+        </div>
+        <div className="flex items-center gap-4">
+          <HeaderIconMenu icons={mobileNavicons} />
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 lg:hidden"
-            >
+          >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
