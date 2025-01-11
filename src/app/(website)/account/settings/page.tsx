7f82674c83636f80c6afceeedc12ecda2bcc2fad
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-11 15:46:07 +0600

    developed: password reset form

--- src/app/(website)/account/settings/page.tsx
+++ src/app/(website)/account/settings/page.tsx
@@ -1,9 +1,11 @@
+import PasswordSettingsContainer from "./_components/password-settings-container";
 import ProfileSettingsContainer from "./_components/profile-settings-container";
 
 const Page = () => {
   return (
     <div className="space-y-[40px]">
       <ProfileSettingsContainer />
+      <PasswordSettingsContainer />
     </div>
   );
 };
