57a2ad31bb4371534de63314fff605e90e139d9e
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-16 17:53:36 +0600

    added: demo data table

---
+++ src/data/demo.ts
@@ -0,0 +1,29 @@
+export interface DemoTableItemsType {
+  image: string;
+  Associate: string;
+  Store: string;
+  Size: string;
+}
+
+export const demoTableItems: DemoTableItemsType[] = [
+  {
+    image: "https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg",
+    Associate: "John Doe",
+    Store: "Example Store 1",
+    Size: "731 KB",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/30224866/pexels-photo-30224866/free-photo-of-serene-woman-relaxing-in-a-pool-with-red-bikini.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Associate: "Jane Smith",
+    Store: "Example Store 2",
+    Size: "512 KB",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/30224866/pexels-photo-30224866/free-photo-of-serene-woman-relaxing-in-a-pool-with-red-bikini.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Associate: "Alice Johnson",
+    Store: "Example Store 3",
+    Size: "1.2 MB",
+  },
+];
