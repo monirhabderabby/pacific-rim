3858104c661f579cd00d69a441b647931b8840e8
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-16 17:05:24 +0600

    developed: reusable dropdown

---
+++ src/components/ui/PacificDropdownSelector.tsx
@@ -0,0 +1,41 @@
+import { PacificDropdownType } from "@/types";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+
+const PacificDropdownSelector = ({
+  list,
+  selectedValue,
+  onValueChange,
+}: {
+  list: PacificDropdownType[]; // List of items
+  selectedValue: string; // Currently selected value
+  onValueChange: (value: string) => void; // Function to handle value change
+}) => {
+  return (
+    <Select
+      value={selectedValue}
+      onValueChange={(val) => onValueChange(val)} // Update the state on selection
+    >
+      <SelectTrigger className="bg-green-600 rounded-[8px] text-white">
+        <SelectValue placeholder={selectedValue} />
+      </SelectTrigger>
+      <SelectContent className="w-fit">
+        <SelectGroup>
+          {list.map((item) => (
+            <SelectItem key={item.id} value={item.value}>
+              {item.name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default PacificDropdownSelector;
