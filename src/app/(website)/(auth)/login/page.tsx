0c8b01e07b77bc6912cf4416975ac5d3371f4b7d
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-17 14:45:54 +0600

    developed: login page

--- src/app/(website)/(auth)/login/page.tsx
+++ src/app/(website)/(auth)/login/page.tsx
@@ -1,12 +1,9 @@
-import { FormProvider } from "@/provider/form-provider";
 import { LoginForm } from "../_components/forms/login-form";
 
 const Page = () => {
   return (
     <div>
-      <FormProvider>
-        <LoginForm />
-      </FormProvider>
+      <LoginForm />
     </div>
   );
 };
