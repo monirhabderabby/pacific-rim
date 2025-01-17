83fd0be0130170a98acc7c8fb60dab9299a183ab
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-17 17:47:41 +0600

    developed: forget password

--- src/app/(website)/(auth)/reset-password/page.tsx
+++ src/app/(website)/(auth)/reset-password/page.tsx
@@ -1,10 +1,10 @@
-import { ResetPasswordForm } from "../_components/forms/reset-password-form";
+import { ForgotPasswordForm } from "../_components/forms/forgot-password-form";
 import AuthUIProvider from "../_components/provider/AuthUIProvider";
 
 const Page = () => {
   return (
     <AuthUIProvider sidebarImage="https://i.postimg.cc/X7ywkKYd/e0ba46c9845debdbdfbf19f3d80aba56-1.png">
-      <ResetPasswordForm />
+      <ForgotPasswordForm />
     </AuthUIProvider>
   );
 };
