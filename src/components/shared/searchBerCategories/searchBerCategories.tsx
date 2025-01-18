c93e692322fc4dc31170df254b675667752f6551
sozibhossain <sozibhossain69@gmail.com>
2025-01-18 14:26:32 +0600

    Header, categories, footer update

--- src/components/shared/searchBerCategories/searchBerCategories.tsx
+++ src/components/shared/searchBerCategories/searchBerCategories.tsx
@@ -6,7 +6,7 @@ import Categories from '../categories/categories';
 
 function SearchBerCategories() {
     return (
-        <div className='bg-primary-light py-5 '>
+        <div className='bg-primary-light py-[10px]'>
             <div className='container lg:flex flex-1 items-center justify-center gap-8'>
                 <div>
                     <Categories />
