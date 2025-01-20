5ac6a01b4a6457c52a9f61024ac915dc66c5ec5a
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-01-20 18:16:49 +0600

    banner section modal set

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,9 +1,53 @@
-// package import 
+"use client"
+// package import
 import Image from "next/image";
 
+import { useState } from "react";
+import Modal from "../shared/modal/modal";
+import { Button } from "../ui/button";
+
 const Hero = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div>
+    <>
+      {isOpen && (
+
+        <Modal>
+        <div>
+          <div className="mx-auto mt-6 w-[160px] md:w-[205px] h-[160px] md:h-[205px] relative">
+            <Image
+              src="/assets/img/logo.png"
+              alt="logo"
+              fill
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+          <h2 className="text-center text-[28px] md:text-[36.53px] text-[#333333] font-bold leading-[33.6px] md:leading-[43.84px] mt-2">
+          PACIFIC RIM FUSION
+          </h2>
+          <h3 className="text-center text-[32px] text-[#0057A8] font-semibold leading-[38.4px] mt-[25px]">
+          Get 50% offer for your first purchase.
+          </h3>
+          <p className="text-center text-[22px] text-[#102011] font-normal leading-[26.4px] mt-4">
+          &quot;Save up on your favorite items - Limited time offer&quot;
+          </p>
+          <div className="w-full flex justify-center mt-[35px]">
+            <Button  onClick={()=>setIsOpen(!isOpen)} className="w-[80%] bg-[#0057A8]">
+            Reward Now
+            </Button>
+          </div>
+          <div className="w-full flex justify-center mt-[14px]">
+            <Button onClick={()=>setIsOpen(isOpen)} variant="outline" className="w-[80%] text-[#0057A8] border border-[#0057A8]">
+            No Thanks
+            </Button>
+          </div>
+        </div>
+      </Modal>
+      )}
+
+
+     {
+      !isOpen &&  <div>
       <section
         className="relative w-full h-[430px] lg:h-[530px] bg-cover bg-center z-[-10] lg:px-20"
         style={{
@@ -23,10 +67,10 @@ const Hero = () => {
               BALANCE
             </h1>
             <p className="text-[16px] md:text-[18px] mb-8">
-              SHRED is here to make your weed experience easier. It all started
-              with three great flavor pre-milled blends. Since then, SHRED has
-              expanded to include gummies, vapes, infused pre-rolls, and even
-              hash!
+              SHRED is here to make your weed experience easier. It all
+              started with three great flavor pre-milled blends. Since then,
+              SHRED has expanded to include gummies, vapes, infused pre-rolls,
+              and even hash!
             </p>
           </div>
         </div>
@@ -36,7 +80,6 @@ const Hero = () => {
       <div className="grid grid-cols-3 gap-4 lg:gap-[24px] mx-auto max-w-[1200px] mt-[-50px] lg:mt-[-100px] z-50 justify-items-center p-2">
         <div className="h-[130px] w-[103px] md:w-[200px] md:h-[220px] lg:h-[300px] lg:w-[370px]  rounded-[16px] relative">
           <Image
-          
             src="/assets/img/hero1.png"
             alt="Picture of the author"
             className="rounded-[16px]"
@@ -66,26 +109,28 @@ const Hero = () => {
 
       <style>
         {`
-          @keyframes moveBackground {
-            0% {
-              background-position: 0 0;
-            }
-            100% {
-              background-position: 100% 100%; /* Ends at this position */
-            }
+        @keyframes moveBackground {
+          0% {
+            background-position: 0 0;
           }
+          100% {
+            background-position: 100% 100%; /* Ends at this position */
+          }
+        }
 
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              opacity: 1;
-            }
-          }       
-        `}
+        @keyframes fadeIn {
+          0% {
+            opacity: 0;
+          }
+          100% {
+            opacity: 1;
+          }
+        }       
+      `}
       </style>
     </div>
+     }
+    </>
   );
 };
 
