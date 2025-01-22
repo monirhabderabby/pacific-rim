eda5f76b480ae6217c396669bc55eaa6c7c203b8
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-22 16:15:43 +0600

    comment: homepage

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,24 +1,15 @@
 // locatl  import ==================
-import FAQSection from "@/components/faq-accordion";
 import Hero from "@/components/hero/Hero";
-import PopularCategories from "@/components/PopularCategories/PopularCategories";
-import { ClientReviews } from "@/components/shared/clientReview/ClientReview";
 import SearchBerCategories from "@/components/shared/searchBerCategories/searchBerCategories";
-import AboutSection from "@/components/shared/sections/about-section";
-import PopularBlogs from "@/components/shared/sections/popularBlogs/popularBlogs";
-import BestOffer from "./_components/best_offer";
-import DealOfTheDay from "./_components/deal_of_the_day";
 
 const Page = () => {
-  const loggedin = true;
-
   return (
     <div>
       <div className="min-h-screen ">
         <SearchBerCategories />
 
         <Hero />
-        <PopularCategories loggedin={loggedin} />
+        {/* <PopularCategories loggedin={loggedin} />
         {!loggedin && (
           <AboutSection image="https://utfs.io/f/HkyicnKv4pLkKb11IfnzkrEA5LwVvWx2Fbfe7a6P94u0gcjZ" />
         )}
@@ -32,7 +23,7 @@ const Page = () => {
         {loggedin && <PopularBlogs />}
         {loggedin && <ClientReviews />}
 
-        {loggedin && <BestOffer />}
+        {loggedin && <BestOffer />} */}
       </div>
     </div>
   );
