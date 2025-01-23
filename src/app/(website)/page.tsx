ee8a2bc0440e19e4983bb9d14995021ff42ec17e
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-23 09:42:02 +0600

    added: PopularCategories AboutSection FAQSection

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,16 +1,20 @@
 // locatl  import ==================
 
+import FAQSection from "@/components/faq-accordion";
 import Hero from "@/components/hero/Hero";
+import PopularCategories from "@/components/PopularCategories/PopularCategories";
 import SearchBerCategories from "@/components/shared/searchBerCategories/searchBerCategories";
+import AboutSection from "@/components/shared/sections/about-section";
 
 const Page = () => {
+  const loggedin = true;
   return (
     <div>
       <div className="min-h-screen ">
         <SearchBerCategories />
 
         <Hero />
-        {/* <PopularCategories loggedin={loggedin} />
+        <PopularCategories loggedin={loggedin} />
         {!loggedin && (
           <AboutSection image="https://utfs.io/f/HkyicnKv4pLkKb11IfnzkrEA5LwVvWx2Fbfe7a6P94u0gcjZ" />
         )}
@@ -18,7 +22,7 @@ const Page = () => {
           <div className="pb-[160px]">
             <FAQSection />
           </div>
-        )} */}
+        )}
         {/* {loggedin && <DealOfTheDay />}
         {loggedin && <PopularBlog />}
         {loggedin && <ClientReviews />}
