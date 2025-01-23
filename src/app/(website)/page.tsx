5aba5b7f22fc0bacc9a2ce642308e4986d280292
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-23 09:44:56 +0600

    added: only populer categories

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,10 +1,8 @@
 // locatl  import ==================
 
-import FAQSection from "@/components/faq-accordion";
 import Hero from "@/components/hero/Hero";
 import PopularCategories from "@/components/PopularCategories/PopularCategories";
 import SearchBerCategories from "@/components/shared/searchBerCategories/searchBerCategories";
-import AboutSection from "@/components/shared/sections/about-section";
 
 const Page = () => {
   const loggedin = true;
@@ -15,14 +13,14 @@ const Page = () => {
 
         <Hero />
         <PopularCategories loggedin={loggedin} />
-        {!loggedin && (
+        {/* {!loggedin && (
           <AboutSection image="https://utfs.io/f/HkyicnKv4pLkKb11IfnzkrEA5LwVvWx2Fbfe7a6P94u0gcjZ" />
         )}
         {!loggedin && (
           <div className="pb-[160px]">
             <FAQSection />
           </div>
-        )}
+        )} */}
         {/* {loggedin && <DealOfTheDay />}
         {loggedin && <PopularBlog />}
         {loggedin && <ClientReviews />}
