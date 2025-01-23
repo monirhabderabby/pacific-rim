a62da5ec8661040aebf5c3d002e4b8f586701581
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-23 09:50:18 +0600

    added: DealOfTheDay PopularBlog ClientReviews BestOffer

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,10 +1,13 @@
 // locatl  import ==================
 
-import FAQSection from "@/components/faq-accordion";
 import Hero from "@/components/hero/Hero";
 import PopularCategories from "@/components/PopularCategories/PopularCategories";
+import { ClientReviews } from "@/components/shared/clientReview/ClientReview";
 import SearchBerCategories from "@/components/shared/searchBerCategories/searchBerCategories";
 import AboutSection from "@/components/shared/sections/about-section";
+import PopularBlog from "@/components/shared/sections/popularBlogs/popularBlogs";
+import BestOffer from "./_components/best_offer";
+import DealOfTheDay from "./_components/deal_of_the_day";
 
 const Page = () => {
   const loggedin = true;
@@ -18,16 +21,16 @@ const Page = () => {
         {!loggedin && (
           <AboutSection image="https://utfs.io/f/HkyicnKv4pLkKb11IfnzkrEA5LwVvWx2Fbfe7a6P94u0gcjZ" />
         )}
-        {!loggedin && (
+        {/* {!loggedin && (
           <div className="pb-[160px]">
             <FAQSection />
           </div>
-        )}
-        {/* {loggedin && <DealOfTheDay />}
+        )} */}
+        {loggedin && <DealOfTheDay />}
         {loggedin && <PopularBlog />}
         {loggedin && <ClientReviews />}
 
-        {loggedin && <BestOffer />} */}
+        {loggedin && <BestOffer />}
       </div>
     </div>
   );
