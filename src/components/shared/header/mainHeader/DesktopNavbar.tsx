25df2ebaf65040995efd5bf6a1c80c72fdc54f9d
sozibhossain <sozibhossain69@mail.com>
2025-01-25 00:55:37 -0800

    added

--- src/components/shared/header/mainHeader/DesktopNavbar.tsx
+++ src/components/shared/header/mainHeader/DesktopNavbar.tsx
@@ -5,13 +5,9 @@ import Link from "next/link";
 
 // Local imports
 import HeaderLogo from "/public/assets/header-logo.png";
-import { cn } from "@/lib/utils";
 
-interface DesktopNavbarProps {
-  pathName: string;
-}
 
-function DesktopNavbar({ pathName }: DesktopNavbarProps) {
+function DesktopNavbar() {
   return (
     <nav
       aria-label="Global"
@@ -30,37 +26,25 @@ function DesktopNavbar({ pathName }: DesktopNavbarProps) {
       <PopoverGroup className="hidden lg:flex lg:gap-x-[36px] ">
         <Link
           href="/"
-          className={cn(
-            "text-[20px] font-medium hover:text-gradient",
-            pathName === "/" ? "text-gradient" : "text-black font-normal"
-          )}
+          
         >
           Home
         </Link>
         <Link
           href="/about"
-          className={cn(
-            "text-[20px] font-normal hover:text-gradient",
-            pathName === "/about" ? "text-gradient" : "text-black font-normal"
-          )}
+          
         >
           About
         </Link>
         <Link
           href="/blogs"
-          className={cn(
-            "text-[20px] font-normal hover:text-gradient",
-            pathName === "/blogs" ? "text-gradient" : "text-black font-normal"
-          )}
+          
         >
           Blog
         </Link>
         <Link
           href="/contact"
-          className={cn(
-            "text-[20px] font-normal hover:text-gradient",
-            pathName === "/contact" ? "text-gradient" : "text-black font-normal"
-          )}
+          
         >
           Contact
         </Link>
--- src/components/shared/header/mainHeader/navbar.tsx
+++ src/components/shared/header/mainHeader/navbar.tsx
@@ -1,23 +1,20 @@
 "use client";
 // Packages
 
-import { usePathname } from "next/navigation";
+
 
 // Local imports
 
 
 import DesktopNavbar from "./DesktopNavbar";
 function Navbar() {
-  const pathName = usePathname();
 
   return (
-    <div
-      
-    >
+    <div>
       <header>
         
         <div className="hidden lg:block">
-          <DesktopNavbar pathName={pathName} />
+          <DesktopNavbar  />
         </div>
       </header>
     </div>
