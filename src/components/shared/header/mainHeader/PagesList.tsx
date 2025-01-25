ba5ce5aa9019e108d391bb1060083c46eeead0b3
sozibhossain <sozibhossain69@mail.com>
2025-01-25 02:55:45 -0800

    added: faq, header

--- src/components/shared/header/mainHeader/PagesList.tsx
+++ src/components/shared/header/mainHeader/PagesList.tsx
@@ -1,25 +1,28 @@
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
 import React, { useState } from "react";
 import PagesModal from "./PagesModal";
 
 const PagesList: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleItemClick = () => {
     setIsModalOpen(true);
+    setIsExpanded(true); // Set expanded to true when the modal opens
   };
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
+    setIsExpanded(false); // Set expanded to false when the modal closes
   };
 
   return (
-    <div className="relative">
+    <div className="">
       <span
-        className="flex items-center gap-1"
+        className="flex items-center gap-1 cursor-pointer"
         onClick={() => handleItemClick()}
       >
-        Pages <ChevronDown className="h-4 w-4" />{" "}
+        Pages {isExpanded ? <ChevronUp className="h-4 w-4 text-black" /> : <ChevronDown className="h-4 w-4 text-black"/>}
       </span>
       <PagesModal
         isOpen={isModalOpen}
