bf1a2804016588c80340d41336375fcfc9125f1d
Kongkon-79 <kongkonjowarder@gmail.com>
2025-01-25 15:10:31 +0600

    dashboard sidebar bg image add

--- src/app/(website)/vendor-dashboard/_components/dashboard-sidebar.tsx
+++ src/app/(website)/vendor-dashboard/_components/dashboard-sidebar.tsx
@@ -22,9 +22,13 @@ const DashSidebar = () => {
   const cancelLogout = () => {
     setShowModal(false);
   };
+
+  // bg-gradient-to-b from-[#FFFFFF] to-[#E2EDF6]
   return (
     <>
-      <div className="h-screen w-[354px] bg-white shadow-[0px_28px_20px_0px_#0000000D] sticky top-[0px] pt-[20px] -mt-24">
+      <div style={{
+        backgroundImage: "url('/assets/img/dashboard_sidebar_bg.svg')",
+      }} className="bg-cover bg-center bg-no-repeat h-full w-[354px] bg-white shadow-[0px_28px_20px_0px_#0000000D] sticky top-[0px] pt-[20px] -mt-24">
         <ScrollArea className="h-full w-full">
           <div className="bg-[#F9FAFD] flex justify-center items-center gap-[12px] w-[266px] h-[95px] shadow-[0px_4px_22px_0px_#D3D8FF99] rounded-[36px] ml-[38px] mt-[32px] mr-[50px] ">
             <Image src="/assets/img/dashboard_logo.png" alt="dashboard_img" width={75} height={75} />
@@ -36,23 +40,25 @@ const DashSidebar = () => {
             {sidebarContents.map((item) => (
               <DashboardSidebarItem key={item.id} item={item} />
             ))}
-            <div className="pl-[38px]">
-              <button
-                onClick={(e) => {
-                  {
-                    e.preventDefault();
-                    handleLogout();
-
-                  }
-                }}
-                className={cn(
-                  " w-full h-[46px] rounded-[4px] pl-[16px] flex items-center gap-[12px] font-medium text-[18px] leading-[21.4px] transition-colors duration-300 bg-transparent text-[#152764] hover:text-[#152764] my-[80px]"
-                )}
-              >
-                <LogOutIcon className="w-[16px] h-[16px]" /> Logout
-              </button>
-            </div>
           </div>
+
+          <div className="pl-[38px] pt-[206px]">
+            <button
+              onClick={(e) => {
+                {
+                  e.preventDefault();
+                  handleLogout();
+
+                }
+              }}
+              className={cn(
+                " w-full h-[46px] rounded-[4px] pl-[16px] flex items-center gap-[12px] font-medium text-[18px] leading-[21.4px] transition-colors duration-300 bg-transparent text-[#152764] hover:text-[#152764] my-[80px]"
+              )}
+            >
+              <LogOutIcon className="w-[16px] h-[16px]" /> Logout
+            </button>
+          </div>
+
         </ScrollArea>
 
       </div>
