27e0885051c6371ef8ed6c6720784ce7ae232a89
sozibhossain <sozibhossain69@mail.com>
2025-01-25 00:21:14 -0800

    navbar add

--- src/components/shared/header/mainHeader/navbar.tsx
+++ src/components/shared/header/mainHeader/navbar.tsx
@@ -1,38 +1,12 @@
-"use client";
-// Packages
 
-import { usePathname } from "next/navigation";
-
-// Local imports
-import { cn } from "@/lib/utils";
-import Hideon from "@/provider/Hideon";
-import DesktopNavbar from "./DesktopNavbar";
 
 
 function Navbar() {
-  const pathName = usePathname();
 
   return (
-    <Hideon
-      routes={[
-        "/age-alert",
-        "/vendor-dashboard",
-        "/login",
-        "/registration",
-        "/reset-password",
-        "/forgot-password",
-      ]}
-      currentPath={pathName}
-    >
-      <header className={cn("bg-white", pathName !== "/" && "border-b-2")}>
-        <div className="lg:hidden">
-          {/* <MobileTabletNavbar loggedin={loggedin} /> */}
-        </div>
-        <div className="hidden lg:block">
-          <DesktopNavbar pathName={pathName} />
-        </div>
-      </header>
-    </Hideon>
+    <div>
+      <h1>Navbar</h1>
+    </div>
   );
 }
 
